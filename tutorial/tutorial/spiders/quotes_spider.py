6d32dd6ff3a40a7a1779b01dc0ba3314138cff29
Anitha <hello.ani25@gmail.com>
2021-08-14 13:21:57 -0400

    Added fukes

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            request = scrapy.Request(url=url, callback=self.parse)
+            request.meta['proxy'] = 'http://127.0.0.1:8118'
+            yield request
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
