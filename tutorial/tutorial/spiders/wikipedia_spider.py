ed9923efb42772969dd8ae80f3f650d3bc10f2f9
Anitha <hello.ani25@gmail.com>
2021-08-19 15:52:30 -0400

    Added files

--- tutorial/tutorial/spiders/wikipedia_spider.py
+++ tutorial/tutorial/spiders/wikipedia_spider.py
@@ -10,19 +10,31 @@ logger = logging.getLogger(__name__)
 
 class WikipediaSpider(scrapy.Spider):
     name = "wikipedia"
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'\
+               'application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'en',
+        'Referrer': 'https://en.wikipedia.org/wiki/Main_Page',
+        'Accept-Encoding': 'gzip, deflate, br'
+}
 
     def readUrls(self):
         root_dir = Path(__file__).parent.parent.parent.parent
-        file_path = os.path.join(root_dir, 'files/companies.txt')
+        file_path = os.path.join(root_dir, 'files/b2.txt')
         file = open(file_path, "r")
         companies = file.readlines()
         for company in companies:
             self.start_urls.append("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&search="+ company +"&namespace=0&limit=10")
 
     def start_requests(self):
+        # count = 0
+
         self.readUrls()
         for url in self.start_urls:
-            request = scrapy.Request(url=url, callback=self.parse)
+            # if count > 15:
+            #    break
+            # count += 1
+            request = scrapy.Request(url=url, callback=self.parse, headers=self.headers)
             request.meta['proxy'] = 'http://127.0.0.1:8118'
             print(" *********************************************")
             yield request
